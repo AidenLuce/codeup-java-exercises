46f80778d05d3ebfcd12c609f71f78483edca59d
Aiden Luce <luceaiden4124@gmail.com>
2022-08-29 16:33:39 -0500

    Java Syntax,Types, and Variables exercises completed

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -1,5 +1,26 @@
 public class HelloWorld {
     public static void main (String[] args) {
         System.out.println("Hello World");
+        int myFavoriteNumber = 12;
+        System.out.println(myFavoriteNumber);
+        double myString = 3.14159;
+        System.out.println(myString);
+        double myNumber = 3.14;
+        System.out.println(myNumber);
+        {
+            int x = 4;
+            System.out.println(x += 5);
+        }
+        {
+            int x = 3;
+            int y = 4;
+            System.out.println(y *= x);
+        }
+        {
+            int x = 10;
+            int y = 2;
+            System.out.println(x /= y);
+            System.out.println(y -= x);
+        }
     }
 }
