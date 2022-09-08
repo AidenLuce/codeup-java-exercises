4ecb880c62e967b4e6e223139a6d35e8cb99ec3a
Aiden Luce <luceaiden4124@gmail.com>
2022-09-08 16:48:12 -0500

    javaControlFlow

---
+++ src/ControlFlowExercises.java
@@ -0,0 +1,25 @@
+public class ControlFlowExercises {
+    public static void main(String[] args){
+
+//        int i = 5;
+//        int x = 100;
+//        long y = 2;
+
+//       while(i <= 15){
+//           System.out.println(i);
+//           i++;
+//       }
+
+//       do{
+//           System.out.println(x);
+//           x -= 5;
+//       } while (x >= -10);
+
+//        do{
+//        System.out.println(y);
+//        y = y * y;
+//        } while (y < 1000000);
+
+
+    }
+}
