b53d5dd5964c0b322d52cfe2c023be43208462cf
Aiden Luce <luceaiden4124@gmail.com>
2022-10-03 15:27:26 -0500

    1.Loop Basics  c.For

--- src/ControlFlowExercises.java
+++ src/ControlFlowExercises.java
@@ -58,18 +58,18 @@ public class ControlFlowExercises {
 //            System.out.println(i);
 //        }
 //
-//        for (int i = 1; i <= 100; i++) {
-//            if (i % 3 == 0 && i % 5 == 0) {
-//                System.out.println("Fizzbuzz");
-//            } else if (i % 3 == 0) {
-//                System.out.println("Fizz");
-//            } else if (i % 5 == 0) {
-//                System.out.println("Buzz");
-//            } else {
-//                System.out.println(i);
-//            }
-//
-//        }
+        for (int i = 1; i <= 100; i++) {
+            if (i % 3 == 0 && i % 5 == 0) {
+                System.out.println("Fizzbuzz");
+            } else if (i % 3 == 0) {
+                System.out.println("Fizz");
+            } else if (i % 5 == 0) {
+                System.out.println("Buzz");
+            } else {
+                System.out.println(i);
+            }
+
+        }
 //        Scanner enterNum = new Scanner(System.in);
 //        System.out.println("What number would you like to go up to? ");
 //        int yourNum = enterNum.nextInt();
