0e06597d17b70fbb5ce1a921d332b31d04fb5fdd
Aiden Luce <luceaiden4124@gmail.com>
2022-10-03 15:30:30 -0500

    2. Fizzbuzz

--- src/ControlFlowExercises.java
+++ src/ControlFlowExercises.java
@@ -51,25 +51,25 @@ public class ControlFlowExercises {
 //            System.out.println(i);
 //        }
 //
-        for (int i = 100; i >= -10; i -= 5) {
-            System.out.println(i);
-        }
-        for (long i = 2; i < 1000000; i *= i) {
-            System.out.println(i);
-        }
-//
-//        for (int i = 1; i <= 100; i++) {
-//            if (i % 3 == 0 && i % 5 == 0) {
-//                System.out.println("Fizzbuzz");
-//            } else if (i % 3 == 0) {
-//                System.out.println("Fizz");
-//            } else if (i % 5 == 0) {
-//                System.out.println("Buzz");
-//            } else {
-//                System.out.println(i);
-//            }
-//
+//        for (int i = 100; i >= -10; i -= 5) {
+//            System.out.println(i);
 //        }
+//        for (long i = 2; i < 1000000; i *= i) {
+//            System.out.println(i);
+//        }
+//
+        for (int i = 1; i <= 100; i++) {
+            if (i % 3 == 0 && i % 5 == 0) {
+                System.out.println("Fizzbuzz");
+            } else if (i % 3 == 0) {
+                System.out.println("Fizz");
+            } else if (i % 5 == 0) {
+                System.out.println("Buzz");
+            } else {
+                System.out.println(i);
+            }
+
+        }
 //        Scanner enterNum = new Scanner(System.in);
 //        System.out.println("What number would you like to go up to? ");
 //        int yourNum = enterNum.nextInt();
