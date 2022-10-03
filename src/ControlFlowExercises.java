ac81f160c109367b5c582938c76ab759e8b3a1cf
Aiden Luce <luceaiden4124@gmail.com>
2022-10-03 15:32:30 -0500

    3. Display table of powers

--- src/ControlFlowExercises.java
+++ src/ControlFlowExercises.java
@@ -37,15 +37,15 @@ public class ControlFlowExercises {
 //            q++;
 //        }
 //
-        do {
-            System.out.println(x);
-            x -= 5;
-        } while (x >= -10);
-
-        do {
-            System.out.println(y);
-            y = y * y;
-        } while (y < 1000000);
+//        do {
+//            System.out.println(x);
+//            x -= 5;
+//        } while (x >= -10);
+//
+//        do {
+//            System.out.println(y);
+//            y = y * y;
+//        } while (y < 1000000);
 
 //        for (int i = 5; i <= 15; i++) {
 //            System.out.println(i);
@@ -58,31 +58,31 @@ public class ControlFlowExercises {
 //            System.out.println(i);
 //        }
 //
-        for (int i = 1; i <= 100; i++) {
-            if (i % 3 == 0 && i % 5 == 0) {
-                System.out.println("Fizzbuzz");
-            } else if (i % 3 == 0) {
-                System.out.println("Fizz");
-            } else if (i % 5 == 0) {
-                System.out.println("Buzz");
-            } else {
-                System.out.println(i);
-            }
-
-        }
-//        Scanner enterNum = new Scanner(System.in);
-//        System.out.println("What number would you like to go up to? ");
-//        int yourNum = enterNum.nextInt();
-//        System.out.println("Here is your table!\n");
-//        System.out.println(
-//                "number | squared | cubed\n" +
-//                        "------ | ------- | -----\n");
-//        for (int i = 1; i <= yourNum; i++) {
-//            int squared = i * i;
-//            int cubed = i * i * i;
+//        for (int i = 1; i <= 100; i++) {
+//            if (i % 3 == 0 && i % 5 == 0) {
+//                System.out.println("Fizzbuzz");
+//            } else if (i % 3 == 0) {
+//                System.out.println("Fizz");
+//            } else if (i % 5 == 0) {
+//                System.out.println("Buzz");
+//            } else {
+//                System.out.println(i);
+//            }
 //
-//            System.out.println(i + "      | " + (squared) + "       | " + (cubed));
 //        }
+        Scanner enterNum = new Scanner(System.in);
+        System.out.println("What number would you like to go up to? ");
+        int yourNum = enterNum.nextInt();
+        System.out.println("Here is your table!\n");
+        System.out.println(
+                "number | squared | cubed\n" +
+                        "------ | ------- | -----\n");
+        for (int i = 1; i <= yourNum; i++) {
+            int squared = i * i;
+            int cubed = i * i * i;
+
+            System.out.println(i + "      | " + (squared) + "       | " + (cubed));
+        }
 //
 //        Scanner userInput = new Scanner(System.in);
 //        boolean noSelection = false;
