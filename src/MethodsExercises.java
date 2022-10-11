31734b5bb1a4909b660687682d46e2d3a6ab3b6b
Aiden Luce <luceaiden4124@gmail.com>
2022-10-11 16:02:19 -0500

    Methods Exercises complete

---
+++ src/MethodsExercises.java
@@ -0,0 +1,25 @@
+public class MethodsExercises {
+    public static void addition (int x, int y){
+        System.out.println(x + y);
+    }
+    public static void subtraction (int x, int y){
+        System.out.println(x - y);
+    }
+    public static void multiplication (int x, int y){
+        System.out.println(x * y);
+    }
+    public static void division (int x, int y){
+        System.out.println(x / y);
+    }
+    public static void modulus (int x, int y){
+        System.out.println(x % y);
+    }
+    public static void main (String [] args){
+        addition(10,8);
+        subtraction(10, 7);
+        multiplication(10, 8);
+        division(10, 5);
+        modulus(100, 8);
+
+    }
+}
