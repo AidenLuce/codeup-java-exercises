ce5e3dc6aa683e99d12cfe54dd9f92a07ff91480
Aiden Luce <luceaiden4124@gmail.com>
2022-10-11 09:48:23 -0500

    String Java Exercise complete

---
+++ src/StringExercise.java
@@ -0,0 +1,15 @@
+public class StringExercise {
+    public static void main (String[] args){
+        String WDNN = "We don't need no";
+        System.out.println(WDNN + " education \n" + WDNN + " thought control");
+
+        String C = "Check ";
+        System.out.println(C + "\"this\" out!, \"s inside of \"s!" );
+
+        String inWindows = "In windows, the main drive is usually ";
+        System.out.println(inWindows + "C:\\");
+
+        String tripleBackslash = "and the amazing triple backslash \\\\\\!";
+        System.out.println("I can do backslashes \\, double backslashes \\\\," + tripleBackslash);
+    }
+}
