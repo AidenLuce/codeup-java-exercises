bce62700865ee30da7ed91b78c8b65fbb9c20d10
Aiden Luce <luceaiden4124@gmail.com>
2022-10-12 14:20:40 -0500

    Dice roll

--- src/DiceRoll.java
+++ src/DiceRoll.java
@@ -1,2 +1,25 @@
+import java.util.Scanner;
+import java.lang.Math;
 public class DiceRoll {
+    public static String roll(){
+        Scanner userInput = new Scanner(System.in);
+        System.out.println("Would you like to roll? (Y/N)");
+        String response = userInput.next();
+        if (response.equalsIgnoreCase("y")){
+            System.out.println("Please enter the number of sides on the dice: ");
+            int num = userInput.nextInt();
+            double dice1 = Math.floor(Math.random() * num) + 1;
+            double dice2 = Math.floor(Math.random() * num) + 1;
+            System.out.println(dice1);
+            System.out.println(dice2);
+            return roll();
+        } else if (response.equalsIgnoreCase("n")){
+            return ("Goodbye");
+        }
+        System.out.println("Invalid response");
+        return roll();
+    }
+    public static void main (String [] args){
+        roll();
+    }
 }
