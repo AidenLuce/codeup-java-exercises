f64b10b79ee820358ce23d3ab54caaffdfe01900
Aiden Luce <luceaiden4124@gmail.com>
2022-10-29 20:42:37 -0500

    ?

--- src/grades/Student.java
+++ src/grades/Student.java
@@ -5,6 +5,7 @@ import java.util.ArrayList;
 public class Student {
     private String name;
     private ArrayList<Integer>grades;
+
 //    public Student(){
 //        this.name = "";
 //        this.grades = new ArrayList<>();
